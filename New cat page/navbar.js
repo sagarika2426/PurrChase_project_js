2e28eeb685e66ebddd2a73b8b5bac8d10ab47d7d
sagarika sahoo <sagarikasahoo16@gmail.com>
2024-01-05 23:04:19 +0530

    navbar component

---
+++ New cat page/navbar.js
@@ -0,0 +1,17 @@
+function navbar(){
+    return `
+    <div id="links">
+    <div><img src="https://us.123rf.com/450wm/zhanna26/zhanna261709/zhanna26170900035/85712562-black-silhouette-of-cat-vector-illustration.jpg?ver=6">
+    <h3>PurrChase</h3></div>
+    <div><a href="home.html">Home</a></div>
+    <div > <a href="products.html">Products</a></div>
+    <div><a href="form.html">Put for Adoption</a></div>
+    <div><a href="Listed.html">Adoption Listing</a></div>
+    <div id="cart_logo"> <a href="cart.html"  style="text-decoration: underline" ><img src="https://cdn-icons-png.flaticon.com/512/3144/3144456.png"></a></div>
+    <div id = "login"><a href="login.html"><img src ="https://i.pinimg.com/originals/c7/ab/cd/c7abcd3ce378191a3dddfa4cdb2be46f.png"></a></div>
+
+  </div>
+    `
+}
+
+export default navbar;
