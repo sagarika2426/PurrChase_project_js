1a6a46a5094613d0845bb20f0af8cf359ef9dcf3
sagarika sahoo <sagarikasahoo16@gmail.com>
2024-01-05 23:10:23 +0530

    Footer component

---
+++ New cat page/footer.js
@@ -0,0 +1,22 @@
+function footer(){
+    return `<div id="footer">
+  <div id="address">
+      <h4>Company Address:</h4>
+      <p>Company Street, City, Country</p>
+  </div>
+
+  <div id="contact">
+      <h4>Contact Number:</h4>
+      <p>+1 (123) 456-7890</p>
+  </div>
+
+  <div id="social-icons">
+      <h4>Follow Us:</h4><br>
+     
+      <a href="" target="_blank"><img src="https://freepngimg.com/save/122260-logo-instagram-free-transparent-image-hq/700x700" alt="Instagram"></a>
+      <a ><img src="https://cdn3.iconfinder.com/data/icons/picons-social/57/46-facebook-512.png" alt="Facebook"></a>
+  </div>
+  </div>`
+}
+
+export default footer;
